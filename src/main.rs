b3cb469ba26aa8deced78158ebc99612e3b32113
Manoel Dantas <manoel.lobo@gmail.com>
2022-08-15 02:08:33 -0300

    feat: Cons list may be reused by multiple Cons lists (Rc)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
 #[derive(Debug)]
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
-use crate::List::{Cons, Nil};
-
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // For `a` to be "shareable", it must be wrapped in a Rc
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 
-    println!("{list:?}");
+    println!("{a:?}");
+    println!("{b:?}");
+    println!("{c:?}");
 }
